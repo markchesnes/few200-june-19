a7dac4924e50cc0ec29522ec183a5b4e6e8b4cbf
markchesnes <markchesnes@users.noreply.github.com>
2019-06-11 17:31:45 +0000

    Connected to todos api

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { TodoListItem } from './models';
+import { TodosDataService } from './todos-data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,23 +9,28 @@ import { TodoListItem } from './models';
   styleUrls: ['./todo-list.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class TodoListComponent {
-  items: TodoListItem[] = [
-    { description: 'Mow Grass', completed: false },
-    { description: 'Clean Gutters', completed: false },
-    { description: 'Change Oil', completed: true }
-  ];
+export class TodoListComponent implements OnInit {
+
+  todos$: Observable<TodoListItem[]>;
+
+  constructor(private service: TodosDataService) {
+    service.getAll();
+  }
+
+  ngOnInit(): void {
+    this.todos$ = this.service.getAll();
+  }
 
   markComplete(item: TodoListItem) {
-    item.completed = true;
+
   }
 
   add(what: string) {
     const description = what;
-    this.items.unshift({ description, completed: false });
+
   }
 
   clearCompleted() {
-    this.items = this.items.filter(item => !item.completed);
+
   }
 }
