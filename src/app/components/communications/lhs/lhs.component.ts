e1b56a6028859b649e4837acdf3e7e18df8ce0ad
markchesnes <markchesnes@users.noreply.github.com>
2019-06-11 14:09:16 +0000

    init communications children component scaffolds

---
+++ src/app/components/communications/lhs/lhs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lhs',
+  templateUrl: './lhs.component.html',
+  styleUrls: ['./lhs.component.css']
+})
+export class LhsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
