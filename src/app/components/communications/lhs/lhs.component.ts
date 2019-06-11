f48f7c1ab7a7f460f959ce072c2b9d661b23aced
markchesnes <markchesnes@users.noreply.github.com>
2019-06-11 14:30:02 +0000

    set up communications service and connect to communications components

--- src/app/components/communications/lhs/lhs.component.ts
+++ src/app/components/communications/lhs/lhs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommunicationsService } from '../communications.service';
 
 @Component({
   selector: 'app-lhs',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LhsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CommunicationsService) { }
 
   ngOnInit() {
+  }
+
+  setIt(what: HTMLInputElement) {
+    this.service.setMessage(what.value);
+    what.value = '';
+    what.focus();
   }
 
 }
