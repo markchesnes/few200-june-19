a1a30385efdee80339bbe49649016ab9a9680356
markchesnes <markchesnes@users.noreply.github.com>
2019-06-13 17:05:41 +0000

    Add count persist by effect

---
+++ src/app/effects/counter.effects.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+import * as counterActions from '../actions/counter.actions';
+
+@Injectable()
+export class CounterEffects {
+
+  // logItOut = createEffect(() =>
+  //   this.actions$
+  //     .pipe(
+  //       tap(a => console.log(a.type))
+  //     ),
+  //   { dispatch: false }
+  // );
+
+  saveSetCountBy = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(counterActions.setCountBy),
+        tap(action => {
+          localStorage.setItem('count-by', action.by.toString());
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(private actions$: Actions) { }
+}
