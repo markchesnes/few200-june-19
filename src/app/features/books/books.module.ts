bd29db7e54c5cd5f4f52daeed30ab55242b8bc31
markchesnes <markchesnes@users.noreply.github.com>
2019-06-13 19:53:21 +0000

    Added scaffold for books list and entry

--- src/app/features/books/books.module.ts
+++ src/app/features/books/books.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books.component';
+import { EntryComponent } from './components/entry/entry.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [BooksComponent],
+  declarations: [
+    BooksComponent,
+    EntryComponent,
+    ListComponent],
   imports: [
     CommonModule
   ]
